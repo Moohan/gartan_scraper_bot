132097d37b6c966b2e5e23848a017cf75f8a63a3
James McMahon <jamesmcmahon0@gmail.com>
2026-01-26 15:44:35 +0000

    Merge requirements and add utility scripts

---
+++ get_id.py
@@ -0,0 +1,6 @@
+import sqlite3
+conn = sqlite3.connect('gartan_availability.db')
+cursor = conn.cursor()
+cursor.execute("SELECT id, name FROM crew WHERE name LIKE '%HAYES%'")
+print(cursor.fetchall())
+conn.close()
